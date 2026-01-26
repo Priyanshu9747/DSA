301f9d01accdd54438bfb59c767a42cbe83a71e3
Priyanshu Sahu <sahupriyanshu9747@gmail.com>
2026-01-27 00:15:28 +0530

    first commit

---
+++ JavaBasics.java
@@ -0,0 +1,182 @@
+/*public class JavaBasics {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}*/
+   
+/*public class JavaBasics {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        System.out.print("Hello World";
+        System.out.print("Hello World");
+    }
+}*/
+
+/*public class JavaBasics {
+    public static void main(String args[]){
+        System.out.println("*@@@ **");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}  */
+
+// public class JavaBasics{
+//     public static void main(String args[]){
+//         int a=5;
+//         int b=10;
+//         System.out.println(a);
+//         System.out.println(b);
+
+//         String name= "Tony Strark";
+//         System.out.println(name);
+//         a=3;
+//         System.out.println(a);
+
+//     }
+
+
+// }
+
+
+
+
+// public class JavaBasics {
+//     public static void main(String args[]){
+//         byte a =8;
+//         char ch='c';
+//         System.out.println(a);
+//         System.out.println(ch);
+//         boolean var= true;
+//         System.out.println(var);
+
+//         int d=2;                       //long
+//         float price=10.4f;             //double
+//         System.out.println(d);
+//         System.out.println(price);
+
+
+//     }
+// }
+    
+
+// public class JavaBasics {
+//     public static void main(String args[]){
+//         //code to calculate sum 
+//         int a=10;
+//         int b=8;
+//         int sum=a+b;
+//         System.out.println(sum);
+//     }
+// }
+
+//  import java.util.*;
+//  public class JavaBasics{
+//      public static void main(String args[]){
+//          Scanner Sc = new Scanner(System.in);
+//         //  String name= Sc.next();
+//         //  System.out.println(name);
+
+//         //  String input = Sc.nextLine();
+//         //  System.out.println(input);
+
+//         // int x= Sc.nextInt();
+//         // System.out.println(x);
+
+//         float price= Sc.nextFloat();
+//         System.out.println(price);
+
+        
+//      }
+//  }
+
+// import java.util.*;
+// public class JavaBasics  {
+//     public static void main(String args[]){
+//       Scanner sc =new Scanner(System.in);
+//       int a= sc.nextInt();
+//       int b= sc.nextInt();
+//     //   int sum=a+b;
+//     //   System.out.println(sum);
+
+//     int product=a*b;
+//     System.out.println(product);
+
+//     }
+// }
+
+// import java.util.*;
+// public class JavaBasics  {
+//      public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("Enter the radius of circle :");
+//         double r=sc.nextDouble();
+//         double Area = 3.14*r*r;
+//         System.out.println("The area of circle is :");
+//         System.out.println(Area);
+
+//      }
+// }
+
+// import java.util.*;
+// public class JavaBasics  {
+//      public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         float number= sc.nextInt();      //Type conversion from int to float
+//         System.out.println(number);
+//     }    
+// }    
+
+// import java.util.*;
+// public class JavaBasics  {
+//   public static void main(String args[]){
+//     Scanner sc=new Scanner(System.in);
+//    // float a=45.765f;
+//    float a=sc.nextFloat(); 
+//    int b=(int)a;             //lossy cnversion
+//     System.out.println(b);
+
+//     char ch='a';
+//     int d=ch;             //type conversion
+//     System.out.println(d);
+//   }
+// }  
+
+
+
+
+
+// import java.util.*;               //type conversion in expression
+// public class JavaBasics  {
+//  public static void main(String args[]){
+//   Scanner sc=new Scanner(System.in);
+//                    char a='a';
+//                    char b='b';
+//                 //char c=a-b;      //not possible   
+//    System.out.println((int)(a));
+//    System.out.println((int)(b));
+//    System.out.println((int)(b-a));
+//     //  short a=5;
+//     //  byte b=45;
+//     //  char c='c';
+//     //  byte bt=(byte)(a+b+c);     //not make any sense   
+        //System.out.println(bt);       
+
+//  }
+// }
+
+// import java.util.*;      //type conversion in expression
+// public class JavaBasics  {
+//  public static void main(String args[]){
+//   Scanner sc=new Scanner(System.in);
+//   int a=5;
+//   float b=3.5f;
+//   long c=15;
+//   double d=30;
+//   double ans=a+b+c+d;
+//   System.out.println(ans);
+
+
+//  }
+// }
