a805e3e15d1dd7965422facd903b3bd06014915a
Priyanshu Sahu <sahupriyanshu.9747@gmail.com>
2026-01-28 23:21:47 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,102 @@
+// ARITHMETIC OPERATORS
+      // 1.  Binary operators
+// import java.util.*;
+// public class operators {
+//     public static void main(String args[]){
+//         int A=35;
+//         int B=20;
+//         System.out.println("the sum of A and B is" +(A+B));
+//         System.out.println("the product of A and B is" +(A*B));
+//         System.out.println("the subtraction of A and B is" +(A-B));
+//         System.out.println("the division of A and B is" +(A/B));
+//         System.out.println("the modulous of A and B is" +(A%B));
+//     }
+// }
+
+
+
+      // 2.  Unary operators
+// import java.util.*;
+// public class operators {
+//     public static void main(String args[]){
+//         // int a=5;
+//         // int b=++a;                       //pre increment
+//         // System.out.println(a);
+//         // System.out.println(b);
+
+//         // int a=5;
+//         // int b=a++;                        //post incrment
+//         // System.out.println(a);
+//         // System.out.println(b);
+
+//         // int a=5;
+//         // int b=--a;                       //pre decrement
+//         // System.out.println(a);
+//         // System.out.println(b);
+
+//         int a=5;
+//         int b=a--;
+//         System.out.println(a);               //post decrement
+//         System.out.println(b);
+
+        
+//     }
+// }
+
+
+
+
+
+//RELATIONAL OPERATORS
+// import java.util.*;
+// public class operators {
+//     public static void main(String args[]){
+//         int a =5;
+//          int b=7;
+//          System.out.println(a==b);
+//          System.out.println(a!=b);
+//          System.out.println(a<b);
+//          System.out.println(a>b);
+//          System.out.println(a<=b);
+//          System.out.println(a>=b);
+//     }
+// }
+
+
+
+// LOGICAL OPERATORS
+//  import java.util.*;
+//  public class operators {
+//      public static void main(String args[]){
+
+//      System.out.println((3>5) && (5>0));   
+//      System.out.println((6>2) && (2>1));         //AND operator
+
+//      System.out.println((3>2)  || (4<2));         //OR operator
+//      System.out.println((1==0)  ||  (3==6));
+
+//      System.out.println(!(false));                // NOT operator
+     
+//      }
+// }
+
+
+
+
+//ASISGNMENT OERATORS(=, += , -= , /+ , %=)
+// import java.util.*;
+//   public class operators {
+//       public static void main(String args[]){
+//         int a=10;
+//         int b=6;
+//         a+=5;           // a=a+5;
+//         b*=2;           //b=b*5;
+//         System.out.println(a);
+
+//         b/=3;            // b=b/3;
+//         System.out.println(b);
+
+
+//       }
+//   }
+
