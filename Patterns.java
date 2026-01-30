a903332ca36da2b9b8ffcbb7e03c7ef032b30eeb
Priyanshu Sahu <sahupriyanshu.9747@gmail.com>
2026-01-30 23:40:55 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,147 @@
+             //SQUARE PATTERNS 
+// import java.util.*;
+// public class Patterns {
+//     public static void main(String args[]){
+//         for(int i=1; i<=4; i++){                     //Outer loop
+//             for(int j=1; j<=4; j++){                  //inner loop
+//                 System.out.print("* ");
+
+//             }
+//             System.out.println("");
+//         }
+
+
+//     }
+
+// }
+
+
+
+
+         // HOLLLOW RECTANGLE PATTERN
+// import java.util.*;
+// public class Patterns {
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int m=sc.nextInt();
+//         for(int i=1; i<=n; i++){
+//             for(int j=1; j<=m; j++){
+//                 if(i==1 || i==n || j==1 || j==m){
+//                     System.out.print("* ");
+//                 }
+//                 else{
+//                     System.out.print("  ");
+//                 }
+                
+//             }
+//             System.out.println("");
+//         }
+
+
+//     }
+
+// }
+
+
+          //Print star pattern
+// import java.util.*;
+// public class Patterns {
+//     public static void main(String args[]){
+//         for(int i=1; i<=4; i++){                    //Outer loop
+//             for(int j=1; j<=i; j++){                  //inner loop
+//                 System.out.print("* ");
+
+//             }
+//             System.out.println("");
+//         }
+
+
+//     }
+
+// }
+
+
+
+
+         //INVERTED STAR PATTERN 
+// import java.util.*;
+// public class Patterns {
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         for(int i=0; i<n; i++){                    //Outer loop
+//             for(int j=0; j<n-i; j++){                  //inner loop
+//                 System.out.print("* ");
+
+//             }
+//             System.out.println("");
+//         }
+
+
+//     }
+
+// }
+
+
+
+         //PRINT HALF PYRAMID PATTERN
+// import java.util.*;
+// public class Patterns {
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         for(int i=1; i<=n; i++){                    //Outer loop
+//             for(int j=1; j<=i; j++){                  //inner loop
+//                 System.out.print(i);
+
+//             }
+//             System.out.println("");
+//         }
+
+
+//     }
+// }
+
+
+
+        //PRINT number PATTERN PYRAMID
+// import java.util.*;
+// public class Patterns {
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int a=1;
+//         for(int i=1; i<=n; i++){                      //Outer loop
+//             for(int j=1; j<=i; j++){                      //inner loop
+//                 System.out.print(a);
+//                 a=a+1;
+
+//             }
+//             System.out.println("");
+//         }
+
+//     }
+// }
+
+
+
+
+               //PRINT CHARACTER PYRAMID PATTERN
+// import java.util.*;
+// public class Patterns {
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         char a='A';
+//         for(int i=1; i<=n; i++){                      //Outer loop
+//             for(int j=1; j<=i; j++){                      //inner loop
+//                 System.out.print(a);
+//                 a=(char)(a+1);                        // also equaks to a++;
+
+//             }
+//             System.out.println("");
+//         }
+
+//     }
+// }
